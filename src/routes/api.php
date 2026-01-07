7a1e57add9dab3da90d2e994e24af5360cffb291
alan0bits <65522085+alan0bits@users.noreply.github.com>
2026-01-07 19:38:05 +0000

    add ponente y asistente

--- src/routes/api.php
+++ src/routes/api.php
@@ -3,6 +3,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventoController;
+use App\Http\Controllers\PonenteController;
+use App\Http\Controllers\AsistenteController;
 
 
 Route::get('/eventos', [EventoController::class, 'index']);
@@ -10,3 +12,5 @@ Route::post('/eventos', [EventoController::class, 'store']);
 Route::get('/eventos/{id}', [EventoController::class, 'show']);
 Route::put('/eventos/{id}', [EventoController::class, 'update']);
 Route::delete('/eventos/{id}', [EventoController::class, 'destroy']);
+Route::apiResource('ponentes', PonenteController::class);
+Route::apiResource('asistentes', AsistenteController::class);
