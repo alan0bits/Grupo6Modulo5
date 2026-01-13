7f8258b999014fe90d230e703fba3def3068881c
alan0bits <65522085+alan0bits@users.noreply.github.com>
2026-01-13 15:19:53 +0000

    instalacion keycloak

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PonenteVistaController;
+
+Route::get('/ponentes-vista',[PonenteVistaController::class,'index'])->name('ponentes.vista');
+Route::post('/ponentes-vista',[PonenteVistaController::class,'store'])->name('ponentes.store');
+Route::delete('/ponentes-vista/{id}', [PonenteVistaController::class, 'destroy'])->name('ponentes.destroy');
 
 Route::get('/', function () {
     return view('welcome');
