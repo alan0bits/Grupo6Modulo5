d401386a1be6e8a006fcad27e472a34d155f75e9
alan0bits <65522085+alan0bits@users.noreply.github.com>
2026-01-13 16:51:05 +0000

    finalizar actividad 5 grupo 6

--- src/config/auth.php
+++ src/config/auth.php
@@ -14,9 +14,10 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
-        'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
+    'guard' => 'api',
+    'passwords' => 'users',
     ],
+
 
     /*
     |--------------------------------------------------------------------------
@@ -36,11 +37,17 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
     ],
+
+    'api' => [
+        'driver' => 'keycloak',
+        'provider' => 'users',
+    ],
+],
+
 
     /*
     |--------------------------------------------------------------------------
